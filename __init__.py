97858ea55657052da341e622ba36848168faef41
g-mecha <maydayfan11@gmail.com>
2024-12-03 09:40:35 +0100

    THe intro sound now plays

--- __init__.py
+++ __init__.py
@@ -102,48 +102,50 @@ class RonjaSkill(OVOSSkill):
     def play_game(self):
         total_rounds = 12
 
+        self.play_audio(f"{self.root_dir}/assets/audio/effects/intro/intro.mp3")
+
         for round_num in range(0, total_rounds + 1):
             self.current_round = round_num
 
             self.gui.show_text(f"Round {round_num}:")
-            self.gui.show_text("Ronja en de piraten", override_idle=True)
+            self.gui.show_text("Raad het geluid", override_idle=True)
 
-            questions, correct_answers, question_audio_files, correct_answer_audio, false_answer_audio, intro, outro, main_question, duration_intro, duration_outro, duration_main, duration_correct, duration_false, duration_answers = self.generate_round_data(round_num)
+            # questions, correct_answers, question_audio_files, correct_answer_audio, false_answer_audio, intro, outro, main_question, duration_intro, duration_outro, duration_main, duration_correct, duration_false, duration_answers = self.generate_round_data(round_num)
 
-            intro_played = False
+            # intro_played = False
 
-            for question, correct_answer, question_audio_file in zip(questions, correct_answers, question_audio_files):
-                if not intro_played and not self.skip_intro:
-                    self.play_intro(intro, duration_intro)
-                    intro_played = True
+            # for question, correct_answer, question_audio_file in zip(questions, correct_answers, question_audio_files):
+            #     if not intro_played and not self.skip_intro:
+            #         self.play_intro(intro, duration_intro)
+            #         intro_played = True
 
-                if intro_played:
-                    self.play_main_question(main_question, duration_main)
+            #     if intro_played:
+            #         self.play_main_question(main_question, duration_main)
 
-                self.play_question_answer(question, question_audio_file, duration_answers)
+            #     self.play_question_answer(question, question_audio_file, duration_answers)
 
-                reply = None
-                while reply not in ['ja', 'nee', 'stop']:
-                    response = self.get_response()
+            #     reply = None
+            #     while reply not in ['ja', 'nee', 'stop']:
+            #         response = self.get_response()
 
-                    if response:
-                        reply = response.lower()
-                    else:
-                        self.speak("Kies maar, ja of nee.")
+            #         if response:
+            #             reply = response.lower()
+            #         else:
+            #             self.speak("Kies maar, ja of nee.")
 
-                if reply == 'ja' and correct_answer:
-                    self.play_correct_answer(correct_answer_audio, duration_correct)
-                    self.play_outro(outro, duration_outro)
-                    break
-                elif (reply == 'ja' and not correct_answer) or (reply == 'nee' and correct_answer):
-                    self.play_false_answer(false_answer_audio, duration_false)
-                    self.play_outro(outro, duration_outro)
-                    break
-                elif reply == 'stop':
-                    return
-                elif round_num == total_rounds:
-                    self.gui.show_text('Einde', override_idle=True)
-                    return
+            #     if reply == 'ja' and correct_answer:
+            #         self.play_correct_answer(correct_answer_audio, duration_correct)
+            #         self.play_outro(outro, duration_outro)
+            #         break
+            #     elif (reply == 'ja' and not correct_answer) or (reply == 'nee' and correct_answer):
+            #         self.play_false_answer(false_answer_audio, duration_false)
+            #         self.play_outro(outro, duration_outro)
+            #         break
+            #     elif reply == 'stop':
+            #         return
+            #     elif round_num == total_rounds:
+            #         self.gui.show_text('Einde', override_idle=True)
+            #         return
 
             self.set_skip_intro(False)
 
