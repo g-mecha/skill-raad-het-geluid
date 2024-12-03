d465e46b69711adf2bbf095052ad54f919d0e566
g-mecha <maydayfan11@gmail.com>
2024-12-03 13:09:10 +0100

    Added quiz questions

--- quiz_data.py
+++ quiz_data.py
@@ -3,9 +3,54 @@ rounds_data = {
         'questions': [
             "Is dit een fietsbel?",
             "Is dit een deurbel?",
-            "Is dit een koebel?"
+            "Is dit een koebel?",
+            "Is dit een kerstbel?"
         ],
-        'correct_answers': [True, False, False],
+        'correct_answers': [True, False, False, False],
         'main_question': "/assets/audio/questions/Fietsbel.mp3",
+    },
+    1: { #Hond
+    'questions': [
+        "Is dit een hond?",
+        "Is dit een kat?",
+        "Is dit een hamster?",
+        "Is dit een slang?",
+        "Is dit een goudvis?",
+    ],
+    'correct_answers': [True, False, False, False, False],
+    'main_question': "/assets/audio/questions/Hond.mp3",
+    },
+    2: { #Broodrooster
+        'questions': [
+            "Is dit een broodrooster?",
+            "Is dit een radio?",
+            "Is dit een stofzuiger?",
+            "Is dit een lamp?",
+            "Is dit een elektrisch deken?"
+        ],
+        'correct_answers': [True, False, False, False, False],
+        'main_question': "/assets/audio/questions/Broodrooster.mp3",
+    },
+    3: { #Helicopter
+        'questions': [
+            "Is dit een helicopter?",
+            "Is dit een vliegtuig?",
+            "Is dit een boot?",
+            "Is dit een race auto?",
+            "Is dit een Tank?"
+        ],
+        'correct_answers': [True, False, False, False, False],
+        'main_question': "/assets/audio/questions/Helicopter.mp3",
+    },
+    4: { #Gitaar
+        'questions': [
+            "Is dit een gitaar?",
+            "Is dit een piano?",
+            "Is dit een trommel?",
+            "Is dit een trompet?",
+            "Is dit een accordeon?",
+        ],
+        'correct_answers': [True, False, False, False, False],
+        'main_question': "/assets/audio/questions/Gitaar.mp4",
     },
 }
